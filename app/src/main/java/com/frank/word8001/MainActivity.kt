2bbc84228385f663af27ea3786dd20b96bfe256c
FrankLiQiang <lq.cn@protonmail.ch>
2023-10-29 20:46:32 +0900

    转为横屏，加时间显示

--- app/src/main/java/com/frank/word8001/MainActivity.kt
+++ app/src/main/java/com/frank/word8001/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.frank.word8001
 
+import android.content.pm.ActivityInfo
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
@@ -17,7 +18,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -29,19 +29,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.frank.word8001.ui.theme.Black
 import com.frank.word8001.ui.theme.Word8001Theme
 import java.util.Timer
 import java.util.TimerTask
+import kotlin.system.exitProcess
 
 private var thisTimer: Timer = Timer()
 private var thisTask: TimerTask? = null
-
+var playTime by mutableStateOf("")
 private val listTime = ArrayList<Int>()
 
 class MainActivity : ComponentActivity() {
@@ -49,6 +54,17 @@ class MainActivity : ComponentActivity() {
     lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+
+        fun getTime(allTime: Int) : String{
+            val a = allTime % 1000
+            val b = (allTime - a) / 1000
+            val c = b % 60
+            val d = (b - c) / 60
+            val e = d % 60
+            val f = (d - e) / 60
+            return "$f:$e:$c"
+        }
         setContent {
             val launcher = rememberLauncherForActivityResult(contract = GetContent()) { result ->
                 mediaPlayer = MediaPlayer().apply {
@@ -65,6 +81,7 @@ class MainActivity : ComponentActivity() {
                             override fun run() {
                                 try {
                                     progress = mediaPlayer.currentPosition.toFloat()
+                                    playTime = "${getTime(progress.toInt())} / ${getTime(mediaPlayer.duration)}"
                                     if (listTime.size == 2) {
                                         if (mediaPlayer.currentPosition > listTime[1]) {
                                             val time: Int = listTime[0]
@@ -119,7 +136,6 @@ class MainActivity : ComponentActivity() {
                         }
                         if (!mediaPlayer.isPlaying) play()
                     },
-                    progress,
                     if (isFirstTime) 200.0f else mediaPlayer.duration.toFloat(),
                     {
                         if (!isFirstTime) mediaPlayer.seekTo(it.toInt())
@@ -135,7 +151,7 @@ class MainActivity : ComponentActivity() {
             MediaPlayer().stop()
             MediaPlayer().release()
         }
-        System.exit(0)
+        exitProcess(0)
     }
 }
 
@@ -151,10 +167,8 @@ fun MainUI(
     replay: () -> Unit,
     previous: () -> Unit,
     next: () -> Unit,
-    progress: Float,
-    maxProgress: Float = 100.0f,
+    maxProgress: Float,
     doSlider: (Float) -> Unit,
-    modifier: Modifier = Modifier
 ) {
 
     Surface(
@@ -164,58 +178,43 @@ fun MainUI(
         Column(Modifier.background(Black), horizontalAlignment = Alignment.CenterHorizontally) {
             Row(
                 modifier = Modifier
-                    .weight(0.5f, true)
+                    .height(60.dp)
                     .padding(3.dp)
             ) {
                 Button(
                     modifier = Modifier
-                        .weight(2f, true)
+                        .weight(1f, true)
                         .fillMaxHeight()
                         .padding(3.dp),
                     onClick = openFile
                 ) {
                     Text("Choose MP3 File")
                 }
-                if (!isFirstTime)
+                if (!isFirstTime) {
+                    Text(
+                        text = playTime,
+                        style = TextStyle(
+                            fontSize = 30.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        ),
+                        modifier = Modifier
+                            .weight(1f, true)
+                            .align(alignment = CenterVertically)
+                            .padding(3.dp)
+                    )
                     Button(
                         modifier = Modifier
-                            .weight(2f, true)
+                            .weight(1f, true)
                             .fillMaxHeight()
                             .padding(3.dp),
                         onClick = replay
                     ) {
                         Text("Replay")
                     }
+                }
             }
-            if (isFirstTime) {
-                Row(modifier = Modifier.weight(6.1f, true)) {}
-            } else {
-                Row(modifier = Modifier.weight(0.1f, true)) {
-                }
-                Slider(
-                    value = progress,
-                    onValueChange = doSlider,
-                    valueRange = 0f..maxProgress,
-                    modifier = Modifier.padding(start = 5.dp, end = 5.dp)
-                )
-                Row(modifier = Modifier.weight(1.1f, true)) {
-                }
-                TabItem(
-                    if (isPlay) R.drawable.outline_pause_circle_outline_24 else R.drawable.outline_play_circle_outline_24,
-                    "Start",
-                    Color.LightGray,
-                    Modifier
-                        .weight(2f, true)
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .weight(2f, true)
-                        .clickable { pause() }
-                )
-                Row(
-                    Modifier
-                        .background(Color.White)
-                        .height(1.dp)
-                        .fillMaxWidth()) {}
+            if (!isFirstTime) {
                 Row(
                     modifier = Modifier
                         .weight(1.1f, true)
@@ -231,11 +230,17 @@ fun MainUI(
                             .weight(1f, true)
                             .clickable { previous() }
                     )
-                    Row(
+                    TabItem(
+                        if (isPlay) R.drawable.outline_pause_circle_outline_24 else R.drawable.outline_play_circle_outline_24,
+                        "Start",
+                        Color.LightGray,
                         Modifier
-                            .background(Color.White)
-                            .width(1.dp)
-                            .fillMaxHeight()) {}
+                            .weight(1f)
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .weight(1f, true)
+                            .clickable { pause() }
+                    )
                     TabItem(
                         R.drawable.outline_skip_next_24, "End",
                         Color.LightGray,
@@ -245,10 +250,15 @@ fun MainUI(
                             .fillMaxHeight()
                             .weight(1f, true)
                             .clickable { next() }
-                            //.background(if (isNext) Color(0xFFFFB6C5FF) else Color.Black)
                             .background(if (isNext) Color.Blue else Color.Black)
                     )
                 }
+                Slider(
+                    value = progress,
+                    onValueChange = doSlider,
+                    valueRange = 0f..maxProgress,
+                    modifier = Modifier.padding(start = 5.dp, end = 5.dp)
+                )
             }
         }
     }
@@ -277,7 +287,7 @@ fun TabItem(@DrawableRes iconId: Int, title: String, tint: Color, modifier: Modi
 fun GreetingPreview() {
     isFirstTime = false
     Word8001Theme {
-        MainUI({}, {}, {}, {}, {}, 0f, 100f, {})
+        MainUI({}, {}, {}, {}, {}, 0f, {})
     }
 }
 
